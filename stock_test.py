993c47b04b557e722ac156dda0c91d92e199ac02
Evan Stanislaw <evanstanislaw@icloud.com>
2022-05-23 21:58:07 -0400

    Changed stock_test
    finished the quote endpoint api testing

--- stock_test.py
+++ stock_test.py
@@ -1,8 +1,18 @@
+'''
+This is test code for the main market program
+
+-The purpose of this code is to get the correct data from the API
+-This code implements the alphavantage APIs for stocks
+
+Documentation for the API
+https://www.alphavantage.co/documentation/
+
+'''
+
 from email.charset import BASE64
 import requests
 from datetime import date
 
-#https://www.alphavantage.co/documentation/
 
 API_KEY = '8E1LCLI81IQHDHAY'
 BASE_URL = 'https://www.alphavantage.co/query'
@@ -10,6 +20,7 @@ function = 'TIME_SERIES_DAILY'
 
 symbol = input("Please enter a symbol: ")
 
+#Daily API
 request_url = f'{BASE_URL}?function={function}&symbol={symbol}&apikey={API_KEY}'
 response = requests.get(request_url)
 
@@ -28,10 +39,19 @@ print("Today's Low: " , low)
 close = round(float(list(today_data.values())[3]),2)
 print("Today's close: " , close)
 
-print("Daily performace:" , round((close-open),2) , "(" + str(round((close/open),2)) + "%)")
 
-url_new = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NIO&apikey={API_KEY}'
+#Quote Endpoint API
+url_new = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}'
 r_new = requests.get(url_new)
 data_new = r_new.json()
 
+data_new = data_new['Global Quote']
 print(data_new)
+
+price = data_new["05. price"]
+change = data_new["09. change"]
+change_percent = data_new["10. change percent"]
+
+print("Today's Price: " , price)
+print("Today's Change: " , change)
+print("Today's Percent Change: " , change_percent)
