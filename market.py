53774944aeb2eb58f54482695eefc99f81b5fdcf
Evan Stanislaw <evanstanislaw@icloud.com>
2022-05-25 17:09:36 -0400

    class changes
    turned the api data extraction into a class method

--- market.py
+++ market.py
@@ -7,37 +7,54 @@ API_KEY = '8E1LCLI81IQHDHAY'
 BASE_URL = 'https://www.alphavantage.co/query'
 
 print("Welcome to Evan's Stock Viewer\n")
-print("Would you like to view cryptocurrency (c) or stocks (s)? ")
-option = input("Which option would you like to choose? ")
 
-while(option != 'c' and option != 's'):
-    print("Invalid input please try again")
+
+continue_var = "yes"
+while continue_var != "no":
+    #main list to hold all of the stocks viewed
+    main_list = []
+
     print("Would you like to view cryptocurrency (c) or stocks (s)? ")
     option = input("Which option would you like to choose? ")
 
-if option == 's':
-    symbol = input("\nWhat is the symbol of the stock? ")
+    #check for valid input
+    while(option != 'c' and option != 's'):
+        print("Invalid input please try again")
+        print("Would you like to view cryptocurrency (c) or stocks (s)? ")
+        option = input("Which option would you like to choose? ")
 
-    request_url = f'{BASE_URL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
-    response = requests.get(request_url)
-
-    data = response.json()
-    if list(data)[0] == "Error Message":
-        print("Invalid symbol please try again")
+    
+    #option for stocks
+    if option == 's':
         symbol = input("\nWhat is the symbol of the stock? ")
 
         request_url = f'{BASE_URL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
         response = requests.get(request_url)
 
         data = response.json()
-    
-    today_data = list(data["Time Series (Daily)"].values())[0]
 
-    open = round(float(list(today_data.values())[0]),2)
-    high = round(float(list(today_data.values())[1]),2)
-    low = round(float(list(today_data.values())[2]),2)
-    close = round(float(list(today_data.values())[3]),2)
+        #checking to make sure the symbol is valid
+        while list(data)[0] == "Error Message":
+            print("Invalid symbol please try again")
+            symbol = input("\nWhat is the symbol of the stock? ")
+
+            url = f'{BASE_URL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
+
+            response = requests.get(url)
+            data = response.json()
+
+        #symbol is valid and continuing
+        open,close,high,low = Stock.time_series_daily(symbol)
+        price,change,change_percent = Stock.global_quote(symbol)
+        
+        #initalize the stock with the values needed
+        stock = Stock(symbol,open,close,high,low,price,change,change_percent)
+        main_list.append(stock)
+
+        stock.print_data()
 
     
+    continue_var = input("Would you like to to view another? (yes/no) ")       
+
 
 
--- stock_class.py
+++ stock_class.py
@@ -1,6 +1,9 @@
 from datetime import date
+from email.charset import BASE64
+import requests
 
 class Stock(object):
+    @classmethod
     def __init__(self, symbol, open, close, high, low, price, change, percent_change):
         self.symbol = symbol
         self.open = open
@@ -11,12 +14,49 @@ class Stock(object):
         self.change = change
         self.percent_change = percent_change
 
+    @classmethod
     def print_data(self):
         today = date.today()
-        print(self.name + "'s data for " + today)
+        print((self.symbol).upper() + "'s data for " + str(today))
+
+    @staticmethod
+    def time_series_daily(symbol):
+        API_KEY = '8E1LCLI81IQHDHAY'
+        BASE_URL = 'https://www.alphavantage.co/query'
+
+        url = f'{BASE_URL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
+        response = requests.get(url)
+
+        data = response.json()
+
+        today_data = list(data["Time Series (Daily)"].values())[0]
+
+        open = round(float(list(today_data.values())[0]),2)
+        high = round(float(list(today_data.values())[1]),2)
+        low = round(float(list(today_data.values())[2]),2)
+        close = round(float(list(today_data.values())[3]),2)
+
+        return (open,high,low,close)
+
+    @staticmethod
+    def global_quote(symbol):
+        API_KEY = '8E1LCLI81IQHDHAY'
+        BASE_URL = 'https://www.alphavantage.co/query'
+
+        url = f'{BASE_URL}?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}'
+        response = requests.get(url)
+
+        data_new = response.json()
+
+        data_new = data_new['Global Quote']
+        price = data_new["05. price"]
+        change = data_new["09. change"]
+        change_percent = data_new["10. change percent"]
+
+        return (price,change,change_percent)
 
 
 class Crypto(Stock):
-    def __init__(self,name, price):
-        super().__init__(name)
+    def __init__(self,symbol, price):
+        super().__init__(symbol)
         self.price = price
