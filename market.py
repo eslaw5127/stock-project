0c63b8e042d183d28df3c37cc0cde7e683f06288
Evan Stanislaw <evanstanislaw@icloud.com>
2022-05-28 14:00:51 -0400

    Main Done
    finished market.py

--- market.py
+++ market.py
@@ -6,26 +6,27 @@ import requests
 API_KEY = '8E1LCLI81IQHDHAY'
 BASE_URL = 'https://www.alphavantage.co/query'
 
-print("Welcome to Evan's Stock Viewer\n")
+#main list to hold all of the stocks viewed
+main_s = []
+main_c = []
 
+print("Welcome to Evan's Stock Viewer\n")
 
 continue_var = "yes"
 while continue_var != "no":
-    #main list to hold all of the stocks viewed
-    main_list = []
 
     print("Would you like to view cryptocurrency (c) or stocks (s)? ")
     option = input("Which option would you like to choose? ")
 
     #check for valid input
-    while(option != 'c' and option != 's'):
+    while(option != "c" and option != "s"):
         print("Invalid input please try again")
         print("Would you like to view cryptocurrency (c) or stocks (s)? ")
         option = input("Which option would you like to choose? ")
 
     
     #option for stocks
-    if option == 's':
+    if option == "s":
         symbol = input("\nWhat is the symbol of the stock? ")
 
         request_url = f'{BASE_URL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
@@ -49,10 +50,11 @@ while continue_var != "no":
         
         #initalize the stock with the values needed
         stock = Stock(symbol,open,close,high,low,price,change,change_percent)
-        main_list.append(stock)
+        main_s.append(stock)
         
         stock.print_data()
 
+    #option for cryptocurrency
     if option == "c":
         symbol = input("What is the symbol for the cryptocurrency? ")
         
@@ -60,6 +62,8 @@ while continue_var != "no":
         response = requests.get(url)
 
         data = response.json()
+
+        #invalid symbol
         while list(data)[0] == "Error Message":
             print("Invalid symbol please try again")
             symbol = input("\nWhat is the symbol of the cryptocurrency? ")
@@ -72,12 +76,50 @@ while continue_var != "no":
         price,name = Crypto.crypto_rates(symbol)
         crypto = Crypto(symbol,price,name)
 
-        main_list.append(crypto)
+        main_c.append(crypto)
 
+        #print out the crypto information
         crypto.print_crypto()
 
+    print()
+    continue_var = input("Would you like to to view another? (yes/no) ")
+    if continue_var == "no":
+        break
+    
+    previous = input("Would you like to view a previous stock? (yes/no) ")
 
-    continue_var = input("Would you like to to view another? (yes/no) ")       
+    #shows a list of the previous stocks
+    if previous == "yes":
+        if(len(main_s) > 0):
+            print()
+            print("Previous stocks: ")
+            for i in range(len(main_s)):
+                print(main_s[i].symbol.upper())
+
+        #list of previous crypto
+        if(len(main_c) > 0):
+            print()
+            print("Previous Crypto: ")
+            for i in range(len(main_c)):
+                print(main_c[i].symbol.upper())
+
+        print()
+        symbol = input("What is the symbol of the stock/crypto? ")
+        symbol = symbol.upper()
+
+        #print the previous data for the stock or crypto
+        for i in range(len(main_s)):
+            if main_s[i].symbol.upper() == symbol:
+                main_s[i].print_data()
+
+        for i in range(len(main_c)):
+            if main_c[i].symbol.upper() == symbol:
+                main_c[i].print_crypto()
+    
+    print()
+    if previous == "yes":
+        continue_var = input("Would you like to to view another? (yes/no) ")
 
 
-
+print()
+print("Thanks, have a good day!")
